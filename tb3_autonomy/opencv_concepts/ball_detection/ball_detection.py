c4325c791ce7ab3f8f1f351ba2ff27fa5c9ba400
Andre Bododea <andrebododea@apptronik.com>
2022-03-18 20:07:04 -0500

    Functional contour generation and drawing on the tennis ball images.

--- tb3_autonomy/opencv_concepts/ball_detection/ball_detection.py
+++ tb3_autonomy/opencv_concepts/ball_detection/ball_detection.py
@@ -23,13 +23,47 @@ def apply_filter(rgb_image, lower_hsv_color_bound, upper_hsv_color_bound):
     # return the binary image after mask has been applied
     return mask
 
-# TODO: FINISH THIS FUNCTION
 def get_all_contours(binary_image_mask):
+    contours, hierarchy = cv2.findContours(binary_image_mask, 
+                                              cv2.RETR_TREE,
+                                              cv2.CHAIN_APPROX_SIMPLE  )
+    return contours
 
+def draw_ball_contour(binary_image, rgb_image, contour_list):
+    black_image = np.zeros([binary_image.shape[0], binary_image.shape[1], 3], 'uint8')
+    index = -1 # specify all contours
+    thickness = 2 # thickness of the contour line
+    green_color = (150, 250, 150) 
+    red_color = (0, 0, 255) 
+    pink_color = (150, 159, 255) 
+    
+    # cv2.drawContours(rgb_image, contour_list, index, color, thickness)
 
-# TODO: FINISH THIS FUNCTION
-def draw_ball_contour(binary_image_mask, rgb_image, contour_list):
+    for c in contour_list:
+        area = cv2.contourArea(c)
+        perimiter = cv2.arcLength(c, True)
+        ((x,y), radius) = cv2.minEnclosingCircle(c)
+        if (area>100):
+            cv2.drawContours(rgb_image, [c], index, green_color, thickness)
+            cv2.drawContours(black_image, [c], index, green_color, thickness)
+            cx, cy = get_contour_center(c) 
+            cv2.circle(rgb_image, (cx, cy), (int)(radius), red_color,1) # draw enclosing circle
+            cv2.circle(black_image, (cx, cy), (int)(radius), red_color,1) # draw enclosing circle
+            cv2.circle(black_image, (cx, cy), 5, pink_color,-1) # draw center
+            print("Area: {}, Perimiter: {}".format(area, perimiter))
 
+    print("Number of contours: {}".format(len(contour_list)))
+    cv2.imshow("RGB Image Contours", rgb_image)  
+    cv2.imshow("Black Image Contours", black_image)  
+
+def get_contour_center(contour):
+    M = cv2.moments(contour)
+    cx = -1
+    cy = -1 
+    if (M['m00'] != 0):
+        cx = int(M['m10']/M['m00'])
+        cy = int(M['m01']/M['m00'])
+    return cx, cy
 
 def main():
     # Step 1: Read the image in
@@ -43,11 +77,9 @@ def main():
     binary_image_mask = apply_filter(rgb_image, lower_hsv_color_bound, upper_hsv_color_bound)
 
     # Step 3: Get the contours
-    # TODO: FINISH THIS FUNCTION
     contour_list = get_all_contours(binary_image_mask)
 
     # Step 4: Draw the contour onto the image
-    # TODO: FINISH THIS FUNCTION
     draw_ball_contour(binary_image_mask, rgb_image, contour_list)
 
     cv2.waitKey(0)
